c4157a69346eeb653a5c9429870a59d8e5619d5c
Naoki <nshiji@smartshopping.co.jp>
2019-10-22 22:42:28 +0900

    コードブロックのシンタックスハイライトを有効化

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,40 @@ module.exports = {
   },
   pathPrefix: '/gatsby-contentful-starter',
   plugins: [
-    'gatsby-transformer-remark',
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-code-titles',
+          },
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              // classPrefix: "language-",
+              // inlineCodeMarker: null,
+              // aliases: {},
+              showLineNumbers: true,
+              // noInlineHighlight: false,
+              // languageExtensions: [
+              //   {
+              //     language: "superscript",
+              //     extend: "javascript",
+              //     definition: {
+              //       superscript_types: /(SuperType)/,
+              //     },
+              //     insertBefore: {
+              //       function: {
+              //         superscript_keywords: /(superif|superelse)/,
+              //       },
+              //     },
+              //   },
+              // ],
+            }
+          }
+        ]
+      }
+    },
     'gatsby-transformer-sharp',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sharp',
